a8c4a4b6ee2a710ad3c2eb241358d7cd4cb5b9b4
john0212 <df850044@gmail.com>
2022-11-03 16:28:58 +0800

    用 thymeleaf 創建 html 檔，並加上 CSS 樣式

---
+++ thymeleafdemo/src/test/java/com/john/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
