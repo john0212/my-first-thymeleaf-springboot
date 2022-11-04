a5eae9c932c4880999de6933fbdb6e7d7b6f20ad
john0212 <df850044@gmail.com>
2022-11-04 15:04:24 +0800

    添加新增員工介面及方法

--- thymeleafdemo/src/main/java/com/john/thymeleafdemo/dao/EmployeeRepository.java
+++ thymeleafdemo/src/main/java/com/john/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,9 +3,14 @@ package com.john.thymeleafdemo.dao;
 import com.john.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code
+
+    // add a method to sort by last name
+    List<Employee> findAllByOrderByLastNameAsc();
 
 }
